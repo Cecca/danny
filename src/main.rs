aa8cb36bdd2f39d6c2be0c5cacd2b1514d074cf0
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-12-17 18:46:06 +0100

    Towards a block iterative cartesian product

--- src/main.rs
+++ src/main.rs
@@ -22,29 +22,30 @@ use timely::dataflow::*;
 use types::{BagOfWords, VectorWithNorm};
 
 fn main() {
-    let mut args = env::args();
-    args.next(); // Skip executable name
-    let measure = args.next().expect("measure is required");
-    let thresh_str = args.next().expect("threshold is required");
-    let thresh = thresh_str
-        .parse::<f64>()
-        .expect(&format!("Error while parsing threshold `{}`", thresh_str));
-    let left_path = args.next().expect("left path is required");
-    let right_path = args.next().expect("right path is required");
+    // let mut args = env::args();
+    // args.next(); // Skip executable name
 
-    let timely_args = "".split_whitespace();
+    // let measure = args.next().expect("measure is required");
+    // let thresh_str = args.next().expect("threshold is required");
+    // let thresh = thresh_str
+    //     .parse::<f64>()
+    //     .expect(&format!("Error while parsing threshold `{}`", thresh_str));
+    // let left_path = args.next().expect("left path is required");
+    // let right_path = args.next().expect("right path is required");
+
+    // let timely_args = "".split_whitespace();
 
     timely::execute_from_args(std::env::args(), move |worker| {
         let index = worker.index();
         let peers = worker.peers() as u64;
         println!("Greetings from worker {} (over {})", index, peers);
 
-        let (mut left, mut right, mut probe) = worker.dataflow(|scope| {
-            let (left_in, left_stream) = scope.new_input::<i32>();
-            let (right_in, right_stream) = scope.new_input::<i32>();
+        let (mut left, mut right, probe) = worker.dataflow(|scope| {
+            let (left_in, left_stream) = scope.new_input();
+            let (right_in, right_stream) = scope.new_input();
             let probe = cartesian(&left_stream, &right_stream, |&x| x as u64, peers)
-                // .inspect_batch(|t, xs| println!("{}: {:?}", t, xs))
-                .inspect(|p| println!("{:?}", p))
+                .inspect_batch(|t, xs| println!("{}: {:?}", t, xs))
+                // .inspect(|p| println!("{:?}", p))
                 .probe();
             // let (out1, out2) = left_stream
             //     .inspect(|x| println!("in1 {}", x))
