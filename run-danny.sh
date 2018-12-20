94654e17897f260391e05abd48aec988d2c85dd1
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-12-20 15:57:21 +0100

    Various improvements to the remote execution script
     - Detach the ssh command
     - Record output on log file
     - Specify complete path to the danny executable
     - Add flag -n to the ssh call
     - Detach from remote processes
     - Use tee to log output

--- run-danny.sh
+++ run-danny.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 ARGS=$@
-DANNY_EXEC=danny
+DANNY_EXEC=~/.cargo/bin/danny
+
+function run_remotely () {
+  ID=$1
+  HOST_PAIR=$2
+  IFS=: read -ra HOST <<< "$HOST_PAIR"
+  HOST=${HOST[0]}
+
+  ssh -n $HOST \
+    "DANNY_PROCESS_ID=$ID DANNY_HOSTS=$DANNY_HOSTS DANNY_THREADS=$DANNY_THREADS $DANNY_EXEC $ARGS | tee /tmp/danny.log"
+}
+
 
 DANNY_THREADS=${DANNY_THREADS:-1}
 if [ -z "$DANNY_HOSTS" ]
@@ -10,14 +21,18 @@ then
   exit 1
 fi
 
-ID=0
-for HOST_PAIR in $(echo $DANNY_HOSTS | tr ',' ' ')
+FIRST_HOST=$(echo $DANNY_HOSTS | tr ',' '\n' | head -n1)
+OTHER_HOSTS=$(echo $DANNY_HOSTS | tr ',' '\n' | sed 1d)
+
+ID=1
+for HOST_PAIR in $OTHER_HOSTS
 do
-  IFS=: read -ra HOST <<< "$HOST_PAIR"
-  HOST=${HOST[0]}
-
-  ssh $HOST \
-    DANNY_PROCESS_ID=$ID DANNY_HOSTS=$DANNY_HOSTS DANNY_THREADS=$DANNY_THREADS $DANNY_EXEC $ARGS
-
+  echo "Spinning up host $ID ( $HOST_PAIR )"
+  # Detach from remote processes
+  run_remotely $ID $HOST_PAIR &
   ID=$(( $ID + 1 ))
 done
+
+echo "Spinning up host 0 ( $FIRST_HOST )"
+run_remotely 0 $FIRST_HOST
+echo "Done"
