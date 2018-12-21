574f8bbb6901e91ed08c20f848e5312656ddbab1
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-12-21 15:55:59 +0100

    First implementation of three way join
    Several optimizations can be made, but this is a first working
    implementation.

--- src/operators.rs
+++ src/operators.rs
@@ -1,14 +1,18 @@
 use abomonation::Abomonation;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::hash::Hash;
 use timely::dataflow::channels::pact::Exchange as ExchangePact;
 use timely::dataflow::channels::pact::ParallelizationContract;
 use timely::dataflow::channels::pushers::tee::Tee;
+use timely::dataflow::operators::capture::{EventLink, Extract, Replay};
 use timely::dataflow::operators::generic::builder_rc::OperatorBuilder;
 use timely::dataflow::operators::generic::OperatorInfo;
 use timely::dataflow::operators::generic::{FrontieredInputHandle, OutputHandle};
 use timely::dataflow::operators::Capability;
-use timely::dataflow::operators::{Concat, ConnectLoop, Enter, Leave, LoopVariable, Map};
+use timely::dataflow::operators::{
+    Concat, ConnectLoop, Enter, Input, Inspect, Leave, LoopVariable, Map, Operator,
+};
 use timely::dataflow::Scope;
 use timely::dataflow::Stream;
 use timely::Data;
@@ -267,4 +271,178 @@ where
 
         result_stream
     }
+}
+
+pub trait ThreeWayJoin<G, K, D1>
+where
+    G: Scope,
+    K: Data + Route + Abomonation + Sync + Send + Clone + Eq + Hash + Debug,
+    D1: Data + Abomonation + Sync + Send + Clone + Debug,
+{
+    /// Return a stream of pairs which is a subset of the center dataset, such that the left key
+    /// comes from the calling stream, and the right key comes from the right stream. Membership of
+    /// pairs to the returned substream is determined by evaluating the given predicate filter on
+    /// pairs of values associated with the keys from the left and the right streams, respectively
+    fn three_way_join<D2, F>(
+        &self,
+        center: &Stream<G, (K, K)>,
+        right: &Stream<G, (K, D2)>,
+        filter: F,
+    ) -> Stream<G, (K, K)>
+    where
+        D2: Data + Abomonation + Sync + Send + Clone + Debug,
+        F: Fn(&D1, &D2) -> bool + 'static;
+}
+
+impl<G, K, D1> ThreeWayJoin<G, K, D1> for Stream<G, (K, D1)>
+where
+    G: Scope,
+    K: Data + Route + Abomonation + Sync + Send + Clone + Eq + Hash + Debug,
+    D1: Data + Abomonation + Sync + Send + Clone + Debug,
+{
+    fn three_way_join<D2, F>(
+        &self,
+        center: &Stream<G, (K, K)>,
+        right: &Stream<G, (K, D2)>,
+        filter: F,
+    ) -> Stream<G, (K, K)>
+    where
+        D2: Data + Abomonation + Sync + Send + Clone + Debug,
+        F: Fn(&D1, &D2) -> bool + 'static,
+    {
+        // State for the left part of the join
+        let mut left_vectors = HashMap::new();
+        let mut right_vectors = HashMap::new();
+        let mut center_pairs = Vec::new();
+        let mut candidate_pairs = Vec::new();
+
+        let candidates = self.binary_frontier(
+            &center,
+            ExchangePact::new(|pair: &(K, D1)| pair.0.route()),
+            ExchangePact::new(|pair: &(K, K)| pair.0.route()),
+            "three-way-join-left",
+            move |_, _| {
+                move |left_in, center_in, output| {
+                    // Accumulate left vectors
+                    // TODO: is there a way to leverage the partitioning from the previous steps?
+                    // Maybe with replaying a stream?
+                    left_in.for_each(|t, data| {
+                        let mut data = data.replace(Vec::new());
+                        let e = left_vectors.entry(t.retain()).or_insert(HashMap::new());
+                        for (k, v) in data.drain(..) {
+                            e.insert(k, v);
+                        }
+                    });
+                    center_in.for_each(|_t, data| {
+                        center_pairs.append(&mut data.replace(Vec::new()));
+                    });
+
+                    for (time, left_map) in left_vectors.iter() {
+                        if !left_in.frontier().less_than(time) {
+                            // We have seen all the values from the left stream
+                            let mut session = output.session(time);
+                            for (l, r) in center_pairs.drain(..) {
+                                session.give((r, (l.clone(), left_map[&l].clone())));
+                            }
+                        }
+                    }
+                    // Cleanup when we have seen all the center vectors for a given timestamp
+                    // i.e. retain the vectors associated with a timestamp such that the frontier
+                    // tells us that we might see someting with a smaller timestamp in the future
+                    left_vectors.retain(|t, _| center_in.frontier().less_than(t));
+                }
+            },
+        );
+
+        let output = candidates.binary_frontier(
+            &right,
+            ExchangePact::new(|pair: &(K, (K, D1))| pair.0.route()),
+            ExchangePact::new(|pair: &(K, D2)| pair.0.route()),
+            "three-way-join-right",
+            move |_, _| {
+                move |candidates_in, right_in, output| {
+                    right_in.for_each(|t, data| {
+                        let mut data = data.replace(Vec::new());
+                        let e = right_vectors.entry(t.retain()).or_insert(HashMap::new());
+                        for (k, v) in data.drain(..) {
+                            e.insert(k, v);
+                        }
+                    });
+
+                    candidates_in.for_each(|_t, data| {
+                        candidate_pairs.append(&mut data.replace(Vec::new()));
+                    });
+
+                    for (time, right_map) in right_vectors.iter() {
+                        if !right_in.frontier().less_than(time) {
+                            // We have seen all the values from the right stream
+                            let mut session = output.session(time);
+                            for (r, (l, lv)) in candidate_pairs.drain(..) {
+                                if filter(&lv, &right_map[&r]) {
+                                    session.give((l, r));
+                                }
+                            }
+                        }
+                    }
+                    right_vectors.retain(|t, _| candidates_in.frontier().less_than(t));
+                }
+            },
+        );
+
+        output
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::mpsc;
+    use std::sync::{Arc, Mutex};
+    use timely::dataflow::operators::{Capture, Inspect, Probe};
+    use timely::dataflow::ProbeHandle;
+
+    #[test]
+    fn test_three_way_join() {
+        let (send, recv) = mpsc::channel();
+        let send = Arc::new(Mutex::new(send));
+        // let conf = timely::Configuration::Process(2).try_build().unwrap();
+        let conf = timely::Configuration::Thread.try_build().unwrap();
+        timely::execute::execute_from(conf.0, conf.1, move |worker| {
+            let send = send.lock().unwrap().clone();
+            let (mut in1, mut in_center, mut in2, mut probe) = worker.dataflow(|scope| {
+                let mut probe = ProbeHandle::new();
+                let (in1, s1) = scope.new_input();
+                let (in_center, stream_center) = scope.new_input();
+                let (in2, s2) = scope.new_input();
+                s1.three_way_join(&stream_center, &s2, |w1: &String, w2: &String| {
+                    w2.starts_with(w1)
+                })
+                .probe_with(&mut probe)
+                .capture_into(send);
+                (in1, in_center, in2, probe)
+            });
+
+            if worker.index() == 0 {
+                in1.send((0, "a".to_owned()));
+                in1.send((1, "b".to_owned()));
+                in1.send((2, "p".to_owned()));
+                in1.send((10, "p".to_owned()));
+                in_center.send_batch(&mut vec![(0, 1), (2, 10)]);
+                in2.send((1, "ciao".to_owned()));
+                in2.send_batch(&mut vec![
+                    (1, "aiuola".to_owned()),
+                    (3, "coriandoli".to_owned()),
+                    (10, "pizza".to_owned()),
+                ]);
+                in1.advance_to(1);
+                in_center.advance_to(1);
+                in2.advance_to(1);
+            }
+            worker.step_while(|| probe.less_than(in1.time()));
+        })
+        .unwrap();
+
+        assert_eq!(vec![(0, vec![(0, 1), (2, 10)])], recv.extract());
+    }
+
 }
