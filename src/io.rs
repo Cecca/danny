5e9163f8c0186e68d4f229c75136a0bd07799e02
Matteo Ceccarello <ceccarel@dei.unipd.it>
2019-01-17 16:22:10 +0100

    Rationalize src/io.rs

--- src/io.rs
+++ src/io.rs
@@ -8,8 +8,6 @@ pub trait ReadDataFile
 where
     Self: std::marker::Sized,
 {
-    // type Item;
-
     fn from_file<F>(path: &PathBuf, mut fun: F) -> ()
     where
         F: FnMut(Self) -> (),
@@ -24,18 +22,28 @@ where
         Self::from_file_partially(path, |_| true, fun);
     }
 
-    fn from_file_partially<P, F>(path: &PathBuf, pred: P, fun: F) -> ()
+    fn from_file_partially<P, F>(path: &PathBuf, pred: P, mut fun: F) -> ()
     where
         P: Fn(u64) -> bool,
-        F: FnMut(u64, Self) -> ();
+        F: FnMut(u64, Self) -> (),
+    {
+        let file = File::open(path).expect("Error opening file");
+        let buf_reader = BufReader::new(file);
+        let mut cnt = 0;
+        for line in buf_reader.lines() {
+            line.and_then(|l| {
+                if pred(cnt) {
+                    let elem = Self::from_line(&l);
+                    fun(cnt, elem);
+                }
+                cnt += 1;
+                Ok(())
+            })
+            .expect("Error processing line");
+        }
+    }
 
-    fn peek_first(path: &PathBuf) -> Self;
-}
-
-impl ReadDataFile for VectorWithNorm {
-    // type Item = VectorWithNorm;
-
-    fn peek_first(path: &PathBuf) -> VectorWithNorm {
+    fn peek_first(path: &PathBuf) -> Self {
         let file = File::open(path).expect("Error opening file");
         let buf_reader = BufReader::new(file);
         let first_line = buf_reader
@@ -43,7 +51,15 @@ impl ReadDataFile for VectorWithNorm {
             .next()
             .expect("Problem reading the first line")
             .expect("Error getting the line");
-        let data: Vec<f64> = first_line
+        Self::from_line(&first_line)
+    }
+
+    fn from_line(line: &str) -> Self;
+}
+
+impl ReadDataFile for VectorWithNorm {
+    fn from_line(line: &str) -> Self {
+        let data: Vec<f64> = line
             .split_whitespace()
             .skip(1)
             .map(|s| {
@@ -53,49 +69,11 @@ impl ReadDataFile for VectorWithNorm {
             .collect();
         VectorWithNorm::new(data)
     }
-
-    fn from_file_partially<P, F>(path: &PathBuf, pred: P, mut fun: F) -> ()
-    where
-        P: Fn(u64) -> bool,
-        F: FnMut(u64, VectorWithNorm) -> (),
-    {
-        let file = File::open(path).expect("Error opening file");
-        let buf_reader = BufReader::new(file);
-        let mut cnt = 0;
-        for line in buf_reader.lines() {
-            line.and_then(|line| {
-                if pred(cnt) {
-                    let data: Vec<f64> = line
-                        .split_whitespace()
-                        .skip(1)
-                        .map(|s| {
-                            s.parse::<f64>()
-                                .expect(&format!("Error parsing floating point number `{}`", s))
-                        })
-                        .collect();
-                    let vec = VectorWithNorm::new(data);
-                    fun(cnt, vec);
-                }
-                Ok(())
-            })
-            .expect("Error processing line");
-            cnt += 1;
-        }
-    }
 }
 
 impl ReadDataFile for BagOfWords {
-    // type Item = BagOfWords;
-
-    fn peek_first(path: &PathBuf) -> BagOfWords {
-        let file = File::open(path).expect("Error opening file");
-        let buf_reader = BufReader::new(file);
-        let first_line = buf_reader
-            .lines()
-            .next()
-            .expect("Problem reading the first line")
-            .expect("Error getting the line");
-        let mut tokens = first_line.split_whitespace().skip(1);
+    fn from_line(line: &str) -> Self {
+        let mut tokens = line.split_whitespace().skip(1);
         let universe = tokens
             .next()
             .expect("Error getting the universe size")
@@ -108,38 +86,5 @@ impl ReadDataFile for BagOfWords {
             })
             .collect();
         BagOfWords::new(universe, data)
-    }
-
-    fn from_file_partially<P, F>(path: &PathBuf, pred: P, mut fun: F) -> ()
-    where
-        P: Fn(u64) -> bool,
-        F: FnMut(u64, BagOfWords) -> (),
-    {
-        let file = File::open(path).expect("Error opening file");
-        let buf_reader = BufReader::new(file);
-        let mut cnt = 0;
-        for line in buf_reader.lines() {
-            line.and_then(|l| {
-                if pred(cnt) {
-                    let mut tokens = l.split_whitespace().skip(1);
-                    let universe = tokens
-                        .next()
-                        .expect("Error getting the universe size")
-                        .parse::<u32>()
-                        .expect("Error parsing the universe size");
-                    let data: Vec<u32> = tokens
-                        .map(|s| {
-                            s.parse::<u32>()
-                                .expect(&format!("Error parsing floating point number `{}`", s))
-                        })
-                        .collect();
-                    let bow = BagOfWords::new(universe, data);
-                    fun(cnt, bow);
-                }
-                cnt += 1;
-                Ok(())
-            })
-            .expect("Error processing line");
-        }
     }
 }
