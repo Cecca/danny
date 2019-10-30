cc0d25654f59c882077815958dfbefd674f53f19
Matteo Ceccarello <mcec@itu.dk>
2019-10-30 16:52:00 +0100

    Fix (again) run.py

--- experiments/run.py
+++ experiments/run.py
@@ -384,7 +384,7 @@ def preprocess_diverse_expansion(base_path, filepath):
     )
 
 def sample_dataset(base_path, filepath):
-    datatype = "cosine" if "glove" in base_path else "jaccard"
+    measure = "cosine" if "glove" in base_path else "jaccard"
     tokens = filepath.split("-")
     size = tokens[-1]
     subprocess.run(
