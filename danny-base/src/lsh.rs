4e0698b16d5d1d3ca356426fa6d49f315e48705e
Matteo Ceccarello <ceccarel@dei.unipd.it>
2020-05-13 18:04:17 +0200

    Fix a bug in the number of repetitions of TensorCollection

--- danny-base/src/lsh.rs
+++ danny-base/src/lsh.rs
@@ -58,8 +58,9 @@ where
         let coll_prob_left = F::probability_at_range(range).powi(k_left as i32);
         let coll_prob_right = F::probability_at_range(range).powi(k_right as i32);
         let repetitions = ((1_f64 - recall.sqrt()).ln().powi(2)
-            / ((1_f64 - coll_prob_left).ln() * (1_f64 - coll_prob_right).ln()))
+            / ((1_f64 - coll_prob_left).ln() * (1_f64 - coll_prob_right).ln())).ceil()
             as usize;
+        assert!(repetitions > 0);
         let part_repetitions = (repetitions as f64).sqrt().ceil() as usize;
         let mut hashers = Vec::new();
         for _ in 0..part_repetitions {
