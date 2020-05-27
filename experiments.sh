32770653a6bdcfcf224d58d22d1dbe9db4f75f71
Matteo Ceccarello <ceccarel@dei.unipd.it>
2020-05-27 15:17:32 +0200

    Add some other experiments

--- experiments.sh
+++ experiments.sh
@@ -2,6 +2,52 @@
 
 set -e 
 
+function baselines() {
+  for BASE_DATA in sift-100nn-0.5 Livejournal Orkut Glove
+  do
+    for THRESHOLD in 0.5 0.7 0.9
+    do
+      DATASET=/mnt/fast_storage/users/mcec/$BASE_DATA-sample-200000.bin
+      echo "Running on $DATASET"
+      test -d $DATASET
+      danny \
+        --hosts ~/hosts.txt \
+        --threads 8 \
+        --threshold $THRESHOLD \
+        --algorithm all-2-all \
+        $DATASET $DATASET
+    done
+  done
+}
+
+function recall() {
+for RECALL in 0.5 0.8 0.9
+do
+  for BASE_DATA in Livejournal Orkut
+  # for BASE_DATA in sift-100nn-0.5 Livejournal Orkut Glove
+  do
+    for ALGORITHM in one-round-lsh hu-et-al
+    do
+      for THRESHOLD in 0.9
+      do
+        DATASET=/mnt/fast_storage/users/mcec/$BASE_DATA-sample-200000.bin
+        echo "Running on $DATASET"
+        test -d $DATASET
+        danny \
+          --hosts ~/hosts.txt \
+          --threads 8 \
+          --threshold $THRESHOLD \
+          --algorithm $ALGORITHM \
+          --recall $RECALL \
+          --sketch-bits 0 \
+          --k 7 \
+          $DATASET $DATASET
+      done
+    done
+  done
+done
+}
+
 function scalability() {
 for BASE_DATA in sift-100nn-0.5 Livejournal Orkut Glove
 do
@@ -11,6 +57,7 @@ do
     do
       DATASET=/mnt/fast_storage/users/mcec/$BASE_DATA-$SIZE_SPEC.bin
       echo "Running on $DATASET"
+      test -d $DATASET
       danny \
         --hosts ~/hosts.txt \
         --threads 8 \
@@ -30,6 +77,7 @@ do
   do
     DATASET=/mnt/fast_storage/users/mcec/$DATA
     echo "Running on $DATASET"
+    test -d $DATASET
     danny \
       --hosts ~/hosts.txt \
       --threads 8 \
@@ -43,5 +91,7 @@ done
 
 }
 
+baselines
+recall
 scalability
 
