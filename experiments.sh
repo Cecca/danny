e0eb821dd4954cf03afd66ae47538f792a804fb2
Matteo Ceccarello <mceccarello@unibz.it>
2021-02-01 15:33:45 +0100

    Add new set of experiments

--- experiments.sh
+++ experiments.sh
@@ -20,6 +20,60 @@ function baselines() {
   done
 }
 
+# This set of experiments searches for the best configuration of parameters for all the different datasets.
+# We don't look at the 
+function search_best() {
+  RECALL=0.8 # <-- required recall
+
+  for BASE_DATA in sift-100nn-0.5 Livejournal Orkut Glove
+  do
+    DATASET=/mnt/fast_storage/users/mcec/$BASE_DATA-sample-200000.bin
+    echo "Running on $DATASET"
+    test -d $DATASET
+
+    for THRESHOLD in 0.7 0.5
+    do
+      for SKETCH_BITS in 0 256 512
+      do
+        for K in 4 8 12
+        do
+          for ALGORITHM in one-round-lsh hu-et-al
+          do
+            danny \
+              --hosts ~/hosts.txt \
+              --threads 8 \
+              --threshold $THRESHOLD \
+              --algorithm $ALGORITHM \
+              --recall $RECALL \
+              --sketch-bits $SKETCH_BITS \
+              --k $K \
+              $DATASET
+          done
+
+          for K2 in 2 4
+          do
+            danny \
+              --hosts ~/hosts.txt \
+              --threads 8 \
+              --threshold $THRESHOLD \
+              --algorithm two-round-lsh \
+              --recall $RECALL \
+              --sketch-bits $SKETCH_BITS \
+              --k $K \
+              --k2 $K2 \
+              $DATASET
+          done
+        done
+      done
+    done
+  done
+
+}
+
+# This set of experiments studies the effect of different _required recalls_.
+# We look at the actual recall that we get when asking for a specific one.
+# Sketching is disabled in this set of experiments in order not to pollute
+# the recall measurement
 function recall() {
 for RECALL in 0.5 0.8 0.9
 do
@@ -27,7 +81,7 @@ do
   do
     for ALGORITHM in one-round-lsh 
     do
-      for THRESHOLD in 0.5 0.7 0.9 
+      for THRESHOLD in 0.5
       do
         DATASET=/mnt/fast_storage/users/mcec/$BASE_DATA-sample-200000.bin
         echo "Running on $DATASET"
@@ -141,8 +195,24 @@ do
 done
 }
 
-baselines
-recall
-#scalability
-# duplicate_removal_cost
+case $1 in
+  baselines)
+    baselines
+    ;;
 
+  best)
+    best
+    ;;
+
+  recall)
+    recall
+    ;;
+
+  scalability)
+    scalability
+    ;;
+
+  dedup)
+    duplicate_removal_cost
+    ;;
+esac
