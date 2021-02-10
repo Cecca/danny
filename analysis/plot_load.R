c52f96253a804d98bbad9c7340ecb4b98bb818ac
Matteo Ceccarello <mceccarello@unibz.it>
2021-02-10 18:45:23 +0100

    Update the load plot

--- analysis/plot_load.R
+++ analysis/plot_load.R
@@ -3,6 +3,7 @@ source("plots.R")
 
 plotdata <- table_load() %>%
     filter(sketch_bits == 0) %>%
+    filter(algorithm %in% c("hu-et-al", "one-round-lsh")) %>%
     group_by(dataset, threshold, algorithm) %>%
     # Select the fastest configuration on each workload
     filter(total_time == min(total_time)) %>%
@@ -10,14 +11,17 @@ plotdata <- table_load() %>%
     filter(threshold == 0.5)
 
 ggplot(plotdata, aes(y = Load, x = worker, fill = algorithm)) +
-    geom_col() +
-    facet_grid(vars(algorithm), vars(dataset), switch = "y") +
-    theme_paper() +
-    theme(
-        axis.text.y = element_blank(),
-        axis.title.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        strip.text.y = element_text(angle = 180)
-    )
+    geom_col(
+        data = function(d) filter(d, algorithm == "hu-et-al"),
+        show.legend = F
+    ) +
+    geom_col(
+        data = function(d) filter(d, algorithm == "one-round-lsh"),
+        mapping = aes(y = -Load),
+        show.legend = F,
+    ) +
+    guides(color=F) +
+    facet_wrap(vars(dataset), ncol=4) +
+    theme_void()
 
 ggsave("imgs/load.png", width = 8, height = 4)
