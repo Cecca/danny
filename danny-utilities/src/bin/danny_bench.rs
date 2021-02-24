26e33297843df79335c39c6ce778518fea4823de
Matteo Ceccarello <mceccarello@unibz.it>
2021-02-24 13:33:59 +0100

    Better benchmark for components

--- danny-utilities/src/bin/danny_bench.rs
+++ danny-utilities/src/bin/danny_bench.rs
@@ -8,68 +8,22 @@ use danny_base::{lsh::*, sketch::Sketcher512};
 use rand::prelude::*;
 use rand_xorshift::XorShiftRng;
 use serde::Deserialize;
-use std::iter::FromIterator;
 use std::time::{Duration, Instant};
 
-fn bench_similarity<T, P>(queries: &[(T, Vec<T>)], predicate: P) -> Vec<Duration>
+fn bench_similarity<T, P>(pairs: &[(T, T)], samples: usize, predicate: P) -> Vec<Duration>
 where
     T: Clone,
     P: Fn(&T, &T) -> bool,
 {
-    let mut sink = 0;
-    let mut estimates: Vec<Duration> = queries
+    let mut sink = false;
+    let mut estimates: Vec<Duration> = pairs
         .iter()
-        .map(|(q, pts)| {
+        .map(|(q, t)| {
             let timer = Instant::now();
-            sink += pts.iter().filter(|x| predicate(q, x)).count();
-            timer.elapsed() / pts.len() as u32
-        })
-        .collect();
-    eprintln!("sink {}", sink);
-
-    estimates.sort_unstable();
-    estimates
-}
-
-fn bench_sketching<T, H>(queries: &[(T, Vec<T>)], sketcher: &Sketcher512<T, H>, predicate: &SketchPredicate<Sketch512>) -> Vec<Duration>
-where
-    T: Clone,
-    H: LSHFunction<Input = T, Output = u32> + Clone,
-{
-    let mut sink = 0;
-    let mut estimates: Vec<Duration> = queries
-        .iter()
-        .map(|(q, pts)| {
-            let q_sketch = sketcher.sketch(q);
-            let sketches: Vec<Sketch512> = pts.iter().map(|p| sketcher.sketch(p)).collect();
-            let timer = Instant::now();
-            sink += sketches.iter().filter(|x| predicate.eval(x, &q_sketch)).count();
-            timer.elapsed() / pts.len() as u32
-        })
-        .collect();
-    eprintln!("sink {}", sink);
-
-    estimates.sort_unstable();
-    estimates
-}
-
-fn bench_deduplicate<T, H>(queries: &[(T, Vec<T>)], hasher: &TensorCollection<H>) -> Vec<Duration>
-where
-    T: Clone,
-    H: LSHFunction<Input = T, Output = u32> + Clone,
-{
-    let mut sink = 0;
-    let mut estimates: Vec<Duration> = queries
-        .iter()
-        .map(|(q, pts)| {
-            let q_pool = hasher.pool(q);
-            let pools: Vec<TensorPool> = pts.iter().map(|v| hasher.pool(v)).collect();
-            let repetitions = hasher.repetitions();
-            let timer = Instant::now();
-            for repetition in 0..repetitions {
-                sink += pools.iter().filter(|p| hasher.already_seen(p, &q_pool, repetition)).count();
+            for _ in 0..samples {
+                sink ^= predicate(q, t);
             }
-            timer.elapsed() / (pts.len() * repetitions) as u32
+            timer.elapsed() / samples as u32
         })
         .collect();
     eprintln!("sink {}", sink);
@@ -78,10 +32,71 @@ where
     estimates
 }
 
+fn bench_sketching<T, H>(
+    pairs: &[(T, T)],
+    samples: usize,
+    sketcher: &Sketcher512<T, H>,
+    predicate: &SketchPredicate<Sketch512>,
+) -> Vec<Duration>
+where
+    T: Clone,
+    H: LSHFunction<Input = T, Output = u32> + Clone,
+{
+    let mut sink = false;
+    let mut estimates: Vec<Duration> = pairs
+        .iter()
+        .map(|(q, t)| {
+            let q_sketch = sketcher.sketch(q);
+            let t_sketch = sketcher.sketch(t);
+            let timer = Instant::now();
+            for _ in 0..samples {
+                sink ^= predicate.eval(&q_sketch, &t_sketch);
+            }
+            timer.elapsed() / samples as u32
+        })
+        .collect();
+    eprintln!("sink {}", sink);
+
+    estimates.sort_unstable();
+    estimates
+}
+
+fn bench_deduplicate<T, H>(
+    pairs: &[(T, T)],
+    samples: usize,
+    hasher: &TensorCollection<H>,
+) -> Vec<Duration>
+where
+    T: Clone,
+    H: LSHFunction<Input = T, Output = u32> + Clone,
+{
+    let mut sink = false;
+    let mut estimates: Vec<Duration> = pairs
+        .iter()
+        .map(|(q, t)| {
+            let q_pool = hasher.pool(q);
+            let t_pool = hasher.pool(t);
+            let repetitions = hasher.repetitions();
+            let mut duration = Duration::from_nanos(0);
+            for repetition in 0..repetitions {
+                let timer = Instant::now();
+                for _ in 0..samples {
+                    sink ^= hasher.already_seen(&q_pool, &t_pool, repetition);
+                }
+                duration += timer.elapsed();
+            }
+            duration / (repetitions * samples) as u32
+        })
+        .collect();
+    eprintln!("sink {}", sink);
+
+    estimates.sort_unstable();
+    estimates
+}
 
 fn bench<T, H, P, B, R>(
     path: String,
-    n_queries: usize,
+    samples: usize,
     n_targets: usize,
     predicate: P,
     builder: B,
@@ -91,101 +106,122 @@ fn bench<T, H, P, B, R>(
 ) where
     for<'de> T: ReadBinaryFile + Deserialize<'de> + SketchEstimate + Clone,
     H: LSHFunction<Input = T, Output = u32> + Clone,
-    P: Fn(&T, &T) -> bool,
+    P: Fn(&T, &T) -> bool + Copy,
     R: Rng + SeedableRng + Send + Sync + Clone + 'static,
     B: Fn(usize, &mut R) -> H + Sized + Send + Sync + Clone + 'static,
 {
     let hasher = TensorCollection::new(8, 0.5, 0.8, builder, rng);
 
-    println!("reading data... ");
+    eprintln!("reading data... ");
     let mut data = Vec::new();
-    ReadBinaryFile::read_binary(path.into(), |_| true, |_, v: T| data.push(v));
+    ReadBinaryFile::read_binary(path.clone().into(), |_| true, |_, v: T| data.push(v));
     data.shuffle(rng);
-    println!("loaded and shuffled data");
+    eprintln!("loaded and shuffled data");
 
-    println!("building close and far lists... ");
-    let queries = &data[..n_queries];
-    let close: Vec<(T, Vec<T>)> = queries
+    eprintln!("building close and far lists... ");
+    let queries = &data[..n_targets];
+    let close: Vec<(T, T)> = queries
         .iter()
-        .map(|q| {
-            (
-                q.clone(),
-                Vec::from_iter(
-                    data.iter()
-                        .filter(|x| predicate(q, x))
-                        .cycle()
-                        .cloned()
-                        .take(n_targets),
-                ),
-            )
+        .flat_map(|q| {
+            let q = q.clone();
+            let q1 = q.clone();
+            data.iter()
+                .filter(move |x| predicate(&q, x))
+                .cloned()
+                .take(n_targets)
+                .map(move |t| (q1.clone(), t))
         })
         .collect();
-    let far: Vec<(T, Vec<T>)> = queries
+    let far: Vec<(T, T)> = queries
         .iter()
-        .map(|q| {
-            (
-                q.clone(),
-                Vec::from_iter(
-                    data.iter()
-                        .filter(|x| !predicate(q, x))
-                        .cycle()
-                        .cloned()
-                        .take(n_targets),
-                ),
-            )
+        .flat_map(|q| {
+            let q = q.clone();
+            let q1 = q.clone();
+            data.iter()
+                .filter(move |x| !predicate(&q, x))
+                .cloned()
+                .take(n_targets)
+                .map(move |t| (q1.clone(), t))
         })
         .collect();
-    println!("done");
+    eprintln!("done");
 
-    println!("Estimating cost of similarity predicate for close points");
-    let similarity_close_estimates = bench_similarity(&close, &predicate);
-    println!(
+    eprintln!("Estimating cost of similarity predicate for close points");
+    let similarity_close_estimates = bench_similarity(&close, samples, &predicate);
+    eprintln!(
         "median time to verify similarity of close points {:?}",
         similarity_close_estimates[similarity_close_estimates.len() / 2]
     );
-    println!("Estimating cost of similarity predicate for far points");
-    let similarity_far_estimates = bench_similarity(&far, &predicate);
-    println!(
+    eprintln!("Estimating cost of similarity predicate for far points");
+    let similarity_far_estimates = bench_similarity(&far, samples, &predicate);
+    eprintln!(
         "average time to verify similarity of far points {:?}",
         similarity_far_estimates[similarity_far_estimates.len() / 2]
     );
 
-    println!("Estimating cost of sketch predicate for close points");
-    let sketch_close_estimates = bench_sketching(&close, &sketcher, &sketch_predicate);
-    println!(
+    eprintln!("Estimating cost of sketch predicate for close points");
+    let sketch_close_estimates = bench_sketching(&close, samples, &sketcher, &sketch_predicate);
+    eprintln!(
         "median time to verify sketch of close points {:?}",
         sketch_close_estimates[sketch_close_estimates.len() / 2]
     );
-    println!("Estimating cost of sketch predicate for far points");
-    let sketch_far_estimates = bench_sketching(&far, &sketcher, &sketch_predicate);
-    println!(
+    eprintln!("Estimating cost of sketch predicate for far points");
+    let sketch_far_estimates = bench_sketching(&far, samples, &sketcher, &sketch_predicate);
+    eprintln!(
         "average time to verify sketch of far points {:?}",
         sketch_far_estimates[sketch_far_estimates.len() / 2]
     );
 
-    println!("Estimating cost of dedup predicate for close points");
-    let dedup_close_estimates = bench_deduplicate(&close, &hasher);
-    println!(
+    eprintln!("Estimating cost of dedup predicate for close points");
+    let dedup_close_estimates = bench_deduplicate(&close, samples, &hasher);
+    eprintln!(
         "median time to dedup close points {:?}",
         dedup_close_estimates[dedup_close_estimates.len() / 2]
     );
 
-    println!("Estimating cost of dedup predicate for far points");
-    let dedup_far_estimates = bench_deduplicate(&far, &hasher);
-    println!(
+    eprintln!("Estimating cost of dedup predicate for far points");
+    let dedup_far_estimates = bench_deduplicate(&far, samples, &hasher);
+    eprintln!(
         "median time to dedup far points {:?}",
         dedup_far_estimates[dedup_far_estimates.len() / 2]
     );
 
+    for (sketch, (dedup, verify)) in sketch_close_estimates.iter().zip(
+        dedup_close_estimates
+            .iter()
+            .zip(similarity_close_estimates.iter()),
+    ) {
+        println!(
+            "{},close,{},{},{}",
+            path,
+            sketch.as_nanos(),
+            dedup.as_nanos(),
+            verify.as_nanos()
+        );
+    }
+
+    for (sketch, (dedup, verify)) in sketch_far_estimates.iter().zip(
+        dedup_far_estimates
+            .iter()
+            .zip(similarity_far_estimates.iter()),
+    ) {
+        println!(
+            "{},far,{},{},{}",
+            path,
+            sketch.as_nanos(),
+            dedup.as_nanos(),
+            verify.as_nanos()
+        );
+    }
 }
 
 fn main() {
     let path = std::env::args().nth(1).expect("missing dataset path");
-    let n_queries = std::env::args()
+    let samples = std::env::args()
         .nth(2)
-        .expect("missing number of queries")
+        .expect("missing number of samples")
         .parse::<usize>()
-        .expect("number of queries should be integer");
+        .expect("number of samples should be integer");
     let n_targets = std::env::args()
         .nth(3)
         .expect("missing number of queries")
@@ -201,7 +237,7 @@ fn main() {
             let dim = Vector::peek_one(path.clone().into()).dim();
             bench(
                 path,
-                n_queries,
+                samples,
                 n_targets,
                 move |a, b| Vector::cosine_predicate(a, b, threshold),
                 Hyperplane::builder(dim),
@@ -212,8 +248,8 @@ fn main() {
         }
         ContentType::BagOfWords => bench(
             path,
-            n_queries,
-                n_targets,
+            samples,
+            n_targets,
             move |a, b| BagOfWords::jaccard_predicate(a, b, threshold),
             OneBitMinHash::builder(),
             Sketch512::from_jaccard(&mut rng),
