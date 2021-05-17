db6f5a97b1962a6ba3e3a3a9a086ac600b87884b
Matteo Ceccarello <ceccarel@dei.unipd.it>
2021-05-17 12:13:18 +0200

    More logging

--- danny/src/join.rs
+++ danny/src/join.rs
@@ -193,6 +193,7 @@ where
                         cur += subproblem_size;
                     }
 
+                    info!("Processor allocations: {:?}", processor_allocations);
                     let mut subproblem_allocations = HashMap::new();
                     for (p, allocs) in processor_allocations.into_iter().enumerate() {
                         for (key, subproblem_key, marker) in allocs {
